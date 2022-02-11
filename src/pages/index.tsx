00ed57faa03e445e08f8b6cd3c9cf8ede66efb2c
Anderson Roque <andersonbarrinha18@gmail.com>
2022-02-11 17:35:53 -0300

    Create SubscribeButton Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
-export default function Home() {
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,11 +25,30 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for R$9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Avatar" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1KS2wLJPO71IkQlZUAPv3Fzx", {
+    expand: ["product"],
+  });
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+  };
+};
